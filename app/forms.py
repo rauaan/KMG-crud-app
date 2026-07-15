83501799609b5d5ac934c44f1bb0b8c4cb7f0561
Rauan <bm.rauan@gmail.com>
2026-07-15 15:16:01 +0500

    fixed bug with editing reports

--- app/forms.py
+++ app/forms.py
@@ -30,6 +30,11 @@ class CreateDailyProduction(FlaskForm):
     water_cut = IntegerField(validators= [InputRequired(), NumberRange(min=0, max=100, message = "обводненность 0-100%%")])
     density = IntegerField(validators= [InputRequired()])
 
+    def __init__(self, *args, original_well_id=None, original_date=None, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.original_well_id = original_well_id
+            self.original_date = original_date
+
     def validate(self, extra_validators=None):
         if not super().validate(extra_validators):
             return False
@@ -39,6 +44,15 @@ class CreateDailyProduction(FlaskForm):
             date=self.date.data
         ).first()
 
+        is_updating_same_record = (
+                    self.original_well_id is not None and 
+                    self.original_date is not None and
+                    self.well_id.data == self.original_well_id and
+                    self.date.data == self.original_date
+                )
+        
+        if is_updating_same_record:
+            return True
 
         if existing_report:
             self.date.errors.append(
